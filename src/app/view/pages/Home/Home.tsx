a405039d5d95004e9bb7cf6cfb2e76b08b82b894
EnricoSantarelli <enricosantarelli@hotmail.com>
2023-09-25 18:49:22 -0300

    arc complete 🚗🚗

--- src/app/view/pages/Home/Home.tsx
+++ src/app/view/pages/Home/Home.tsx
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 import { useContext } from "react";
-=======
-import { useState } from "react";
->>>>>>> cdfc9916ede7d974fd18781d7347515350da00af
 import { Student } from "../../../model/student";
 import Button from "../../components/Button/Button";
 import "./Home.css";
@@ -12,7 +8,6 @@ import { HomeContext } from "../../../context/HomeContext";
 export default function Home() {
   const { student, setStudent, index, setIndex } = useContext(HomeContext)!;
 
-<<<<<<< HEAD
   const randomNumber = Math.floor(Math.random() * 5);
 
   if (randomNumber <= 2) {
@@ -35,31 +30,6 @@ export default function Home() {
       ) : (
         <NotFound />
       )}
-=======
-interface HomeProps {
-  student: Student;
-  handleButtonClick: () => void;
-}
-
-export default function Home({ student, handleButtonClick }: HomeProps) {
-
-  const [index, setIndex] = useState(0);
-
-  const handleClick = () => {
-    handleButtonClick();
-    setIndex(index + 1);
-  }
-
-  return (
-    <body>
-      <div>
-        <h1>{student.name}</h1>
-        <p>{student.ra}</p>
-        <p>O aluno está trabalhando? {student.isWorking ? "SIM" : "NÃO"}</p>
-      </div>
-      <Button buttonTitle="Entrar" handleButtonClick={handleClick} />
-      <p>{index}</p>
->>>>>>> cdfc9916ede7d974fd18781d7347515350da00af
     </body>
   );
 }
